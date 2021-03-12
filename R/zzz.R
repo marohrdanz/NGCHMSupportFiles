7f02d4b33f8518ba0377434955af917b44167fac
Mary Rohrdanz <marohrdanz@mdanderson.org>
2021-03-12 10:17:26 -0600

    Fix package name in onLoad

--- R/zzz.R
+++ R/zzz.R
@@ -4,8 +4,8 @@
 ## from this package when this package is loaded.
 
 .onLoad <- function(libname, pkgname) {
-	Sys.setenv('SHAIDYMAPGEN' = system.file('java','ShaidyMapGen.jar', package='ShaidyMapGenJar'))
-	Sys.setenv('NGCHMWIDGETPATH' = system.file('js','ngchmWidget-min.js', package='ShaidyMapGenJar'))
+	Sys.setenv('SHAIDYMAPGEN' = system.file('java','ShaidyMapGen.jar', package='NGCHMSupportFiles'))
+	Sys.setenv('NGCHMWIDGETPATH' = system.file('js','ngchmWidget-min.js', package='NGCHMSupportFiles'))
 	message(paste('Set environment variable SHAIDYMAPGEN to ', Sys.getenv('SHAIDYMAPGEN'),sep=''))
 	message(paste('Set environment variable NGCHMWIDGETPATH to ', Sys.getenv('NGCHMWIDGETPATH'),sep=''))
 }
